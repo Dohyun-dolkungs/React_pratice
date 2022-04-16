b6c0cc4f6046299e9865fc399d735d70c1f72961
Dolkungs <dpj177tf@gmail.com>
2022-04-17 02:46:15 +0900

    JSX grammar

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import Hello from './Hello';
+import './App.css';
 
 function App() {
+  const name = 'react';
+  
+  // style should be applied by const
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24,
+    padding: '1rem'
+  };
+
   return (
-    <div>
-      <Hello />  
-      <Hello />  
-      <Hello />  
-    </div>
+    // use fragment tag<> to cover more than 2 tags
+    <>
+      {/* JSX grammar => always tag should be closed */}
+      <Hello />
+      
+      {/* use values by using {} */}
+      <div style={style}>{name}</div> 
+      
+      {/* Use className than class */}
+      <div className="gray-box"></div>
+    </>
   );
 }
 
