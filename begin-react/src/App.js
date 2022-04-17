7f1795c67dca6ef3bf1fa25b5e62f21bd590a0a3
Dolkungs <dpj177tf@gmail.com>
2022-04-17 11:29:19 +0900

    how to manage input

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import Counter from './Counter';
+import InputSample from './InputSameple';
+
 
 function App() {
   return (
-    <Counter />
+    <InputSample/>
   )
 }
 
