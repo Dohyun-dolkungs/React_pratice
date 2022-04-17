1bfb10bafdccd7af45e34c89951568e0ccc833e4
Dolkungs <dpj177tf@gmail.com>
2022-04-17 11:20:26 +0900

    useState()

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import Hello from './Hello';
-import Wrapper from './Wrapper';
+import Counter from './Counter';
 
 function App() {
   return (
-    <Wrapper>
-      {/* conditional rendering => if you forgot to state boolean variable than it's ture */}
-      <Hello name = "react" color = "green" isSpecial/>
-      <Hello color="pink"/> 
-    </Wrapper>
+    <Counter />
   )
 }
 
