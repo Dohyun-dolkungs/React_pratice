7fdf9547afc7f6cf8239b01ae03585ca35f84279
Dolkungs <dpj177tf@gmail.com>
2022-04-19 15:49:59 +0900

    How to use const

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import InputSample from './InputSameple';
+import UserList from './UserList';
+import './App.css';
+
 
 
 function App() {
+  const name = 'react';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24,
+    padding: '1rem'
+  };
+
   return (
-    <InputSample/>
+    <>
+    <UserList />
+    <div style = {style}>{name}</div>
+    <div className='gray-box'></div>
+    </>
   )
 }
 
---
+++ begin-react/src/sample.js
@@ -0,0 +1,17 @@
+const users = [
+    {
+        id: 1,
+        username: 'Dohyun',
+        email: 'public.dohyun@gmail.com'
+    },
+    {
+        id: 2,
+        username: 'tester',
+        email: 'tester@example.com'
+    },
+    {
+        id: 3,
+        username: 'liz',
+        email: 'liz@exmaple.com'
+    }
+];
