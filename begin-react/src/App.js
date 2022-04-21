a834c500899b20fb6d36aa928b0879c8c2c24d05
Dolkungs <dpj177tf@gmail.com>
2022-04-21 10:24:23 +0900

    how to make var don't rerendering when the value changed

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import UserList from './UserList';
-import './App.css';
-
-
 
 function App() {
   
+  const users = [
+    {
+        id: 1,
+        username: 'Dohyun',
+        email: 'public.dohyun@gmail.com'
+    },
+    {
+        id: 2,
+        username: 'tester',
+        email: 'tester@example.com'
+    },
+    {
+        id: 3,
+        username: 'liz',
+        email: 'liz@exmaple.com'
+    }
+  ];
+  
+  const nextId = useRef(4);
+
+  const onCreate = () => {
+    console.log(nextId.current); // 4
+    nextId.current += 1;
+  }
 
   return (
   
-    <UserList />
+    <UserList users={users} />
   
   
   )
